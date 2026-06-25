7c9260063ae67f90723e99a565bf6775da2ca29d
kuznetsov-radio <51828521+kuznetsov-radio@users.noreply.github.com>
2026-06-25 11:47:32 +0800

    Add files via upload

--- source/Plasma.cpp
+++ source/Plasma.cpp
@@ -6,25 +6,30 @@ int FindPlasmaDispersion(double f, double fpe, double fBe, double st, double ct,
 {
  int res=1;
 
- double u=sqr(fBe/f);
- double v=sqr(fpe/f);
-
- double D2=sqr(u*sqr(st))+4.0*u*sqr((1.0-v)*ct);
- if (D2<0) res=0;
+ double fc=(sigma==1) ? fpe : fBe/2+sqrt(fpe*fpe+fBe*fBe/4);
+ if (f<=fc) res=0;
  else
  {
-  double D=sqrt(D2);
+  double u=sqr(fBe/f);
+  double v=sqr(fpe/f);
 
-  double N2=1.0-2.0*v*(1.0-v)/(2.0*(1.0-v)-u*sqr(st)+sigma*D);
-  if (N2<0) res=0;
+  double D2=sqr(u*sqr(st))+4.0*u*sqr((1.0-v)*ct);
+  if (D2<0) res=0;
   else
   {
-   *N=sqrt(N2);
-   *T=2.0*sqrt(u)*(1.0-v)*ct/(u*sqr(st)-sigma*D);
-   *L=(v*sqrt(u)*st+(*T)*u*v*st*ct)/(1.0-u-v+u*v*sqr(ct));
-   *Zh=u ? 2.0*(u*sqr(st)+2.0*sqr(1.0-v)-sigma*sqr(u*sqr(st))/D)/sqr(2.0*(1.0-v)-u*sqr(st)+sigma*D) : 1.0;
+   double D=sqrt(D2);
 
-   res=finite(*N) && finite(*T) && finite(*L) && finite(*Zh);
+   double N2=1.0-2.0*v*(1.0-v)/(2.0*(1.0-v)-u*sqr(st)+sigma*D);
+   if (N2<0) res=0;
+   else
+   {
+    *N=sqrt(N2);
+    *T=2.0*sqrt(u)*(1.0-v)*ct/(u*sqr(st)-sigma*D);
+    *L=(v*sqrt(u)*st+(*T)*u*v*st*ct)/(1.0-u-v+u*v*sqr(ct));
+    *Zh=u ? 2.0*(u*sqr(st)+2.0*sqr(1.0-v)-sigma*sqr(u*sqr(st))/D)/sqr(2.0*(1.0-v)-u*sqr(st)+sigma*D) : 1.0;
+
+    res=finite(*N) && finite(*T) && finite(*L) && finite(*Zh);
+   }
   }
  }
 
